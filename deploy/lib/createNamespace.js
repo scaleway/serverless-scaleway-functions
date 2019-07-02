621a105cc0bd74bc3a81d6cf81e7a532f6fc36bd
amottier <amottier@online.net>
2019-07-02 17:48:52 +0200

    patch function/NS, show errors

--- deploy/lib/createNamespace.js
+++ deploy/lib/createNamespace.js
@@ -15,14 +15,21 @@ module.exports = {
 
   createIfNotExists(foundNamespace) {
     // If Space already exists -> Do not create
+
+    if (foundNamespace && foundNamespace.status === 'error') {
+      throw new Error(foundNamespace.error_message)
+    }
+
     const isReady = foundNamespace && foundNamespace.status === 'ready';
     if (isReady) {
       this.saveNamespaceToProvider(foundNamespace);
+      this.updateNamespace(foundNamespace);
       return BbPromise.resolve();
     }
 
     if (foundNamespace && !isReady) {
       this.serverless.cli.log('Waiting for Namespace to become ready...');
+      this.updateNamespace(foundNamespace);
       return this.waitNamespaceIsReady();
     }
 
@@ -35,12 +42,18 @@ module.exports = {
 
     return this.provider.apiManager.post('namespaces', params)
       .then(response => this.saveNamespaceToProvider(response.data))
+      .catch((err) => {
+        throw new Error(err.response.data.message)
+      })
       .then(() => this.waitNamespaceIsReady());
   },
 
   waitNamespaceIsReady() {
     return this.provider.apiManager.get(`namespaces/${this.namespace.id}`)
       .then((response) => {
+        if (response.data.status == 'error') {
+          throw new Error(response.data.error_message)
+        }
         if (response.data.status !== 'ready') {
           return new Promise((resolve) => {
             setTimeout(() => resolve(this.waitNamespaceIsReady()), 1000);
@@ -49,4 +62,17 @@ module.exports = {
         return true;
       });
   },
+
+  updateNamespace(foundNamespace) {
+    if (this.namespaceVariables) {
+
+      const params = {};
+      params.environment_variables = this.namespaceVariables
+
+      return this.provider.apiManager.patch(`namespaces/${foundNamespace.id}`, params)
+        .catch((err) => {
+          throw new Error(err.response.data.message)
+        })
+    }
+  }
 };
