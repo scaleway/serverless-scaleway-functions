ffc9c69af28f6a7fd6479457eee0f30f5d9da54d
norbjd <norbjd@users.noreply.github.com>
2022-04-15 14:25:08 +0200

    Handle secret values from env

--- shared/secrets.js
+++ shared/secrets.js
@@ -12,25 +12,40 @@ module.exports = {
       }));
   },
 
+  resolveSecretValue(key, value) {
+    const re = /^\${([^}]*)}$/;
+    const found = value.match(re);
+
+    if (!found) {
+      return value;
+    }
+
+    if (process.env[found[1]] === null || process.env[found[1]] === undefined) {
+      console.warn(`Env var ${found[1]} used in secret ${key} does not exist: this secret will not be created.`);
+    }
+
+    return process.env[found[1]];
+  },
+
   async mergeSecretEnvVars(existingSecretEnvVars, newSecretEnvVars) {
     const existingSecretEnvVarsByKey = new Map(existingSecretEnvVars.map(
       i => [i.key, i.hashed_value],
     ));
     const newSecretEnvVarsByKey = new Map(newSecretEnvVars.map(
-      i => [i.key, i.value],
+      i => [i.key, this.resolveSecretValue(i.key, i.value)],
     ));
 
     const result = [];
 
     for (const [key, hashedValue] of existingSecretEnvVarsByKey) {
       if (newSecretEnvVarsByKey.get(key) === undefined || newSecretEnvVarsByKey.get(key) === null) {
-        console.debug('Secret:', key, 'have been removed');
+        // secret is removed
         result.push({ key, value: null });
       } else { // exists in both
         const hashMatches = await argon2.verify(hashedValue, newSecretEnvVarsByKey.get(key));
 
         if (!hashMatches) {
-          console.debug('Secret: value of', key, 'has changed');
+          // secret has changed
           result.push({ key, value: newSecretEnvVarsByKey.get(key) });
         }
 
@@ -38,8 +53,8 @@ module.exports = {
       }
     }
 
+    // new secrets
     newSecretEnvVarsByKey.forEach((value, key) => {
-      console.debug('Secret:', key, 'is new');
       result.push({ key, value });
     });
 
