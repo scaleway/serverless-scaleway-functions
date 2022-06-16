be7ca6a611755aad195732e3efcb84535d7f8050
ttacquet <ttacquet@scaleway.com>
2022-06-16 17:59:01 +0200

    add container case for sls info

--- info/lib/display.js
+++ info/lib/display.js
@@ -1,16 +1,27 @@
-"use strict"
+"use strict";
 
 module.exports = {
   displayInfo() {
-    this.getNamespaceFromList(this.serverless.configurationInput.service)
-      .then((namespace) => {
-        // Todo : container case
-        this.listFunctions(namespace.id)
-          .then((functions) => {
-            functions.forEach((func) => {
-              this.serverless.cli.log(JSON.stringify(func, null, '\t'));
-            });
+    const configInput = this.serverless.configurationInput;
+
+    this.getNamespaceFromList(configInput.service).then((namespace) => {
+      if (
+        configInput.service.custom &&
+        configInput.service.custom.containers &&
+        Object.keys(configInput.service.custom.containers).length !== 0
+      ) {
+        this.listContainers(namespace.id).then((containers) => {
+          containers.forEach((container) => {
+            this.serverless.cli.log(JSON.stringify(container, null, "\t"));
           });
-      });
+        });
+      } else {
+        this.listFunctions(namespace.id).then((functions) => {
+          functions.forEach((func) => {
+            this.serverless.cli.log(JSON.stringify(func, null, "\t"));
+          });
+        });
+      }
+    });
   },
 };
