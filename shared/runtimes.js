94ccae052b681602c796ea8192632689740589c1
thomas-tacquet <thomas@tacquet.net>
2022-08-19 12:06:05 +0200

    typo

--- shared/runtimes.js
+++ shared/runtimes.js
@@ -2,7 +2,7 @@
 
 const RUNTIME_STATUS_AVAILABLE = 'available';
 const RUNTIME_STATUS_EOS = 'end_of_support';
-const RUNTIME_STATUS_EOL = 'end_of_live';
+const RUNTIME_STATUS_EOL = 'end_of_life';
 
 module.exports = {
   RUNTIME_STATUS_AVAILABLE,
