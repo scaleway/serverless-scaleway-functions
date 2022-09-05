67873748e610a93d192b321d8a130dffe4475379
thomas-tacquet <thomas@tacquet.net>
2022-09-05 17:11:23 +0200

    refracto code between functions and containers

--- shared/api/domain.js
+++ shared/api/domain.js
@@ -4,34 +4,6 @@ const { manageError } = require("./utils");
 
 module.exports = {
   /**
-   * listDomains is used to read all domains of a wanted function.
-   * @param {Number} functionId the id of the function to read domains.
-   * @returns a Promise with request result.
-   */
-  listDomainsFunction(functionId) {
-    const domainsUrl = `domains?function_id=${functionId}`;
-
-    return this.apiManager
-      .get(domainsUrl)
-      .then((response) => response.data.domains)
-      .catch(manageError);
-  },
-
-  /**
-   * listDomains is used to read all domains of a wanted container.
-   * @param {Number} containerId the id of the container to read domains.
-   * @returns a Promise with request result.
-   */
-   listDomainsContainer(containerId) {
-    const domainsUrl = `domains?container_id=${containerId}`;
-
-    return this.apiManager
-      .get(domainsUrl)
-      .then((response) => response.data.domains)
-      .catch(manageError);
-  },
-
-  /**
    * createDomain is used to call for domain creation, warning : this
    * function does not wait for the domain
    * to be ready.
@@ -48,8 +20,8 @@ module.exports = {
 
   /**
    * deleteDomains is used to destroy an existing domain by it's ID.
-   * @param {Number} domainID ID of the selected domain. 
-   * @returns 
+   * @param {Number} domainID ID of the selected domain.
+   * @returns
    */
   deleteDomain(domainID) {
     const updateUrl = `domains/${domainID}`;
@@ -59,67 +31,4 @@ module.exports = {
       .then((response) => response.data)
       .catch(manageError);
   },
-
-  /**
-   * Waiting for all domains to be ready on a function
-   * @param {UUID} functionId
-   * @returns
-   */
-  waitDomainsAreDeployedFunction(functionId) {
-    return this.listDomainsFunction(functionId)
-      .then((domains) => {
-        let domainsAreReady = true;
-
-        for (let i = 0; i < domains.length; i += 1) {
-          const domain = domains[i];
-
-          if (domain.status === 'error') {
-            throw new Error(domain.error_message);
-          }
-
-          if (domain.status !== 'ready') {
-            domainsAreReady = false;
-            break;
-          }
-        }
-        if (!domainsAreReady) {
-          return new Promise((resolve) => {
-            setTimeout(() => resolve(this.waitDomainsAreDeployedFunction(functionId)), 5000);
-          });
-        }
-        return domains;
-      });
-  },
-
-  /**
-  * Waiting for all domains to be ready on a container
-  * @param {UUID} containerId
-  * @returns
-  */
-  waitDomainsAreDeployedContainer(containerId) {
-    return this.listDomainsContainer(containerId)
-      .then((domains) => {
-        let domainsAreReady = true;
-
-        for (let i = 0; i < domains.length; i += 1) {
-          const domain = domains[i];
-
-          if (domain.status === 'error') {
-            throw new Error(domain.error_message);
-          }
-
-          if (domain.status !== 'ready') {
-            domainsAreReady = false;
-            break;
-          }
-        }
-        if (!domainsAreReady) {
-          return new Promise((resolve) => {
-            setTimeout(() => resolve(this.waitDomainsAreDeployedContainer(containerId)), 5000);
-          });
-        }
-        return domains;
-      });
-  },
-  
 };
