ab2648c79a43384fca2a163804f3a4f26d897809
thomas-tacquet <thomas@tacquet.net>
2022-11-14 16:09:29 +0100

    add rust folder example

---
+++ examples/rust/src/handler.rs
@@ -0,0 +1,10 @@
+use hyper::{
+    Body, Request, Response, StatusCode,
+};
+
+pub fn handle(req: Request<Body>) -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from("Ferris says hello !"))
+        .unwrap()
+}
