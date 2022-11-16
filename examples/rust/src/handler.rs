ef1f31d786e00e94e53fb5f3430ae089c73d143a
Thomas TACQUET <thomas@tacquet.net>
2022-11-16 15:57:52 +0100

    Update examples/rust/src/handler.rs
    Co-authored-by: norbjd <norbjd@users.noreply.github.com>

--- examples/rust/src/handler.rs
+++ examples/rust/src/handler.rs
@@ -5,6 +5,6 @@ use hyper::{
 pub fn handle(req: Request<Body>) -> Response<Body> {
     Response::builder()
         .status(StatusCode::OK)
-        .body(Body::from("Ferris says hello !"))
+        .body(Body::from("Ferris says hello!"))
         .unwrap()
 }
