984e18c41d8961bd6902fc5ddfc363f490d99103
Emilie BOUIN <ebouin@scaleway.com>
2023-05-02 16:56:44 +0200

    tests: remove afterAll clean-up temporarily

--- tests/setup-tests.js
+++ tests/setup-tests.js
@@ -4,4 +4,4 @@ const { afterAll } = require('@jest/globals');
 const { removeAllTestProjects } = require('./utils/clean-up');
 jest.setTimeout(500000);
 
-afterAll(() => removeAllTestProjects());
+//afterAll(() => removeAllTestProjects());
