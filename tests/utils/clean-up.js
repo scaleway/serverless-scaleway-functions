55a4b92ad5044ca4fe40fbf21d69012acb037908
Emilie BOUIN <ebouin@scaleway.com>
2023-05-04 12:14:30 +0200

    tests: refacto clean-up

--- tests/utils/clean-up.js
+++ tests/utils/clean-up.js
@@ -14,21 +14,17 @@ const removeAllTestProjects = async () => {
     if (project.name.includes('test-slsframework-')) {
       projectId = project.id;
       process.env.SCW_DEFAULT_PROJECT_ID = projectId;
-      try {
-        await removeProjectById(projectId);
-      } catch (err) {
-      }
+      await removeProjectById(projectId)
+        .catch();
     }
   }
 }
 
 const removeProjectById = async (projectId) => {
   process.env.SCW_DEFAULT_PROJECT_ID = projectId;
-  try {
-    await removeAllTestNamespaces();
-    accountApi.deleteProject(projectId);
-  } catch (err) {
-  }
+  await removeAllTestNamespaces()
+    .then(() => accountApi.deleteProject(projectId))
+    .catch(() => console.log("pb 2"));
 }
 
 const removeAllTestNamespaces = async () => {
@@ -36,36 +32,31 @@ const removeAllTestNamespaces = async () => {
     const functionApi = new FunctionApi(FUNCTIONS_API_URL + `/${region}`, process.env.SCW_SECRET_KEY);
     const functions = await functionApi.listNamespaces(projectId);
     for (const functionSrv of functions) {
-      try {
-        await functionApi.deleteNamespace(functionSrv.id);
-        await functionApi.waitNamespaceIsDeleted(functionSrv.id);
-      } catch (err) {
-      }
+      await functionApi.deleteNamespace(functionSrv.id)
+        .then(async() => await functionApi.waitNamespaceIsDeleted(functionSrv.id))
+        .catch(() => console.log("pb 3"));
     }
 
     const containerApi = new ContainerApi(CONTAINERS_API_URL + `/${region}`, process.env.SCW_SECRET_KEY);
     const containers = await containerApi.listNamespaces();
     for (const container of containers) {
-      try {
-        await containerApi.deleteNamespace(container.id);
-        await containerApi.waitNamespaceIsDeleted(container.id);
-      } catch (err) {
-      }
+      await containerApi.deleteNamespace(container.id)
+        .then(async () => await containerApi.waitNamespaceIsDeleted(container.id))
+        .catch(() => console.log("pb 4"));
     }
 
-    /*const registryApi = new RegistryApi(REGISTRY_API_URL + `/${region}`, process.env.SCW_SECRET_KEY);
+    const registryApi = new RegistryApi(REGISTRY_API_URL + `/${region}`, process.env.SCW_SECRET_KEY);
     const registries = await registryApi.listRegistryNamespace();
     for (const registry of registries) {
-      try {
-        await registryApi.deleteRegistryNamespace(registry.id);
-      } catch (err) {
-      }
-    }*/
+      await registryApi.deleteRegistryNamespace(registry.id)
+        .catch(() => console.log("pb 5"))
+    }
   }
 }
 
-removeAllTestProjects();
+removeAllTestProjects()
+  .catch(() => console.log("An error occurred during clean-up"));
 
-module.exports = {
-  removeAllTestProjects
+module.exports = async () => {
+  await removeAllTestProjects;
 };
