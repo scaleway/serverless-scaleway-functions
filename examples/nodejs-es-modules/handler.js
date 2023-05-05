38124970f2b2782cfce6a0085db265c66f104f10
Emilie BOUIN <ebouin@scaleway.com>
2023-05-05 12:29:32 +0200

    tests: add local testing to node examples

--- examples/nodejs-es-modules/handler.js
+++ examples/nodejs-es-modules/handler.js
@@ -7,3 +7,10 @@ function handle(event, context, cb) {
     statusCode: 200,
   };
 }
+
+/* This is used to test locally and will not be executed on Scaleway Functions */
+if (process.env.NODE_ENV === 'test') {
+  import("@scaleway/serverless-functions").then(scw_fnc_node => {
+    scw_fnc_node.serveHandler(handle, 8080);
+  });
+}
