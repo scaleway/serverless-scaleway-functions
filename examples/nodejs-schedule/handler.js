16b54c96d47d0188ac3019bec29a695f4733515c
Emilie BOUIN <48752456+Bemilie@users.noreply.github.com>
2023-05-05 15:27:34 +0200

    tests: add local testing to node examples (#160)

--- examples/nodejs-schedule/handler.js
+++ examples/nodejs-schedule/handler.js
@@ -12,3 +12,10 @@ module.exports.handle = (event, context, callback) => {
   // either return cb(undefined, response) or return response
   return response;
 };
+
+/* This is used to test locally and will not be executed on Scaleway Functions */
+if (process.env.NODE_ENV === 'test') {
+  import("@scaleway/serverless-functions").then(scw_fnc_node => {
+    scw_fnc_node.serveHandler(exports.handle, 8080);
+  });
+}
