7ed97ee2bfe7f56d49188651aa901d48ea88452c
Emilie BOUIN <ebouin@scaleway.com>
2023-06-16 14:37:25 +0200

    fix: lint

--- examples/nodejs-schedule/handler.js
+++ examples/nodejs-schedule/handler.js
@@ -1,16 +1,16 @@
 module.exports.handle = (event, context, callback) => {
   // The scheduled event data is held in a JSON string in the body field
-  var eventBody = JSON.parse(event.body)
+  var eventBody = JSON.parse(event.body);
 
   // Log the event data
-  console.log("foo = " + eventBody.foo)
-  console.log("bar = " + eventBody.bar)
+  console.log("foo = " + eventBody.foo);
+  console.log("bar = " + eventBody.bar);
 
   // Return a success response
   const response = {
     statusCode: 200,
     headers: { "Content-Type": ["application/json"] },
-    body: JSON.stringify({message: "Hello from scaleway functions"}),
+    body: JSON.stringify({ message: "Hello from scaleway functions" }),
   };
 
   return response;
