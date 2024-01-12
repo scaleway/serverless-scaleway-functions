15a26fa293bde5cb8674349e2f093ad619cc960f
norbjd <norbjd@users.noreply.github.com>
2024-01-12 11:47:58 +0100

    Use v3 Account API everywhere

--- shared/constants.js
+++ shared/constants.js
@@ -4,8 +4,7 @@ const FUNCTIONS_API_URL = "https://api.scaleway.com/functions/v1beta1/regions";
 const CONTAINERS_API_URL =
   "https://api.scaleway.com/containers/v1beta1/regions";
 const REGISTRY_API_URL = "https://api.scaleway.com/registry/v1/regions";
-const ACCOUNT_API_URL = "https://api.scaleway.com/account/v2/projects";
-const ACCOUNT_V3_API_URL = "https://api.scaleway.com/account/v3/projects";
+const ACCOUNT_API_URL = "https://api.scaleway.com/account/v3/projects";
 const DEFAULT_REGION = "fr-par";
 
 const PRIVACY_PRIVATE = "private";
@@ -15,7 +14,6 @@ module.exports = {
   CONTAINERS_API_URL,
   REGISTRY_API_URL,
   ACCOUNT_API_URL,
-  ACCOUNT_V3_API_URL,
   PRIVACY_PRIVATE,
   DEFAULT_REGION,
 };
